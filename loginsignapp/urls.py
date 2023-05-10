4bf1317b0c8da95bb21e2db637d615819ac9a3bb
roy <jayanthroyg9595@gmail.com>
2023-05-10 10:08:14 +0530

    last commit

--- loginsignapp/urls.py
+++ loginsignapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('signup/',views.signup, name='signup'),
-    path('signin/',views.signin, name='signin'),
-    path('signout/',views.signout, name='signout')
+    path('signup',views.signup, name='signup'),
+    path('signin',views.signin, name='signin'),
+    path('signout',views.signout, name='signout')
 ]
--- loginsignapp/views.py
+++ loginsignapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate,login
 
 def home(request):
     return render(request, "crudapp/index.html")
@@ -18,8 +19,8 @@ def signup(request):
         pass2=request.POST['pass2']
         
         myuser=User.objects.create_user(username, email, password)
-        myuser.firstname= firstname
-        myuser.lastname= lastname
+        myuser.first_name= firstname
+        myuser.last_name= lastname
         
         myuser.save()
         
@@ -31,6 +32,18 @@ def signup(request):
     
     return render(request, "crudapp/signup.html")
 def signin(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user = authenticate(username=username, password=password)
+        
+        if user is not None:
+            login(request,user)
+            fname1= user.last_login
+            return render(request, "crudapp/index.html",{'fname':fname1} )
+        else:
+            messages.error(request,"Incorrect Credentials")
+            return redirect('home')
     return render(request, "crudapp/signin.html")
 def signout(request):
     pass
